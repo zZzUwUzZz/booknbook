be50fed977dc29f04c3a7834a863347c387cceec
zZzUwUzZz <htrue521@gmail.com>
2023-08-25 15:00:26 +0900

    230825

---
+++ bnb/src/main/java/com/cjcs/bnb/BnbApplication.java
@@ -0,0 +1,13 @@
+package com.cjcs.bnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnbApplication.class, args);
+	}
+
+}
---
+++ bnb/src/main/java/com/cjcs/bnb/controller/BnbController.java
@@ -0,0 +1,15 @@
+package com.cjcs.bnb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+ 
+@Controller
+public class BnbController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+ 
+}

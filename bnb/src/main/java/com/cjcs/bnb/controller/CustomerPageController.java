bc6345526dd513ed12fc27b8a6dbac255f437be9
Nilla-va <bahnnn@naver.com>
2023-08-31 13:44:03 +0900

    "한거없는데"

--- bnb/src/main/java/com/cjcs/bnb/controller/CustomerPageController.java
+++ bnb/src/main/java/com/cjcs/bnb/controller/CustomerPageController.java
@@ -1,29 +1,20 @@
 package com.cjcs.bnb.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.cjcs.bnb.service.MemberService;
-import com.cjcs.bnb.service.OrderService;
 import com.cjcs.bnb.service.PurchaseService;
 import com.cjcs.bnb.service.RentalService;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+
 
 @Slf4j
 @Controller
 @RequestMapping("/mypage")
 public class CustomerPageController {
-    
-    //의존성주입
-
-    @Autowired
-    private MemberService mSer;
 
     @Autowired
     private PurchaseService pSer;

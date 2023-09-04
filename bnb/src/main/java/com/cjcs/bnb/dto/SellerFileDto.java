c50fafbefcbec320669888466707146a460b5a73
zZzUwUzZz <htrue521@gmail.com>
2023-09-04 09:26:55 +0900

    잠깐만

--- bnb/src/main/java/com/cjcs/bnb/dto/SellerFileDto.java
+++ bnb/src/main/java/com/cjcs/bnb/dto/SellerFileDto.java
@@ -5,10 +5,8 @@ import lombok.Data;
 
 @Data
 public class SellerFileDto {
-
 	private int sf_id;           // 파일번호
 	private String sf_s_id;      // 판매자아이디
 	private String sf_oriname;   //  --a.jpg (원본파일명)
-	private String sf_sysname;   //  --329840284093.jpg (서버에올린파일명)
-	
+	private String sf_sysname;   //  --329840284093.jpg (서버에 올라간 파일명)
 }

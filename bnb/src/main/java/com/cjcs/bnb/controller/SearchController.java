360aa87fe5fb68b6f48652077ea96e95668ae5a0
zZzUwUzZz <htrue521@gmail.com>
2023-09-04 17:17:25 +0900

    집 레츠고

--- bnb/src/main/java/com/cjcs/bnb/controller/SearchController.java
+++ bnb/src/main/java/com/cjcs/bnb/controller/SearchController.java
@@ -28,8 +28,6 @@ public class SearchController {
     @Autowired
     private BookService bookService;
 
-
-
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public String searchBooks(@RequestParam("keyword") String keyword, @RequestParam(defaultValue = "1") int page,
             Model model) {
@@ -54,40 +52,31 @@ public class SearchController {
         return "/search/search";
     }
 
-
-
-
     @RequestMapping(value = "/map", method = RequestMethod.GET, params = "keyword")
     public String search(@RequestParam String keyword, Model model) {
         List<SellerDto> results = searchService.searchBookstores(keyword);
         int totalItems = searchService.countBookstores(keyword);
 
-
-  // 이미지 정보를 불러와서 별도의 List에 추가
-  List<SellerFileDto> imageInfos = new ArrayList<>();
-  for (SellerDto seller : results) {
-      List<SellerFileDto> images = searchService.getImagesBySellerId(seller.getS_id());
-      if (images != null && !images.isEmpty()) {
-          imageInfos.addAll(images); // 이미지 정보를 리스트에 추가
-      }
-  }
+        // 이미지 정보를 불러와서 별도의 List에 추가
+        List<SellerFileDto> imageInfos = new ArrayList<>();
+        for (SellerDto seller : results) {
+            List<SellerFileDto> images = searchService.getImagesBySellerId(seller.getS_id());
+            if (images != null && !images.isEmpty()) {
+                imageInfos.addAll(images); // 이미지 정보를 리스트에 추가
+            }
+        }
 
         model.addAttribute("keyword", keyword);
         model.addAttribute("results", results);
         model.addAttribute("totalItems", totalItems);
-        model.addAttribute("imageInfos", imageInfos);  // 이미지 정보를 추가
- 
+        model.addAttribute("imageInfos", imageInfos); // 이미지 정보를 추가
+
         System.out.println("검색한 키워드: " + keyword);
         System.out.println("검색 정보: " + results);
         System.out.println("검색 수: " + totalItems);
         System.out.println("이미지 주소: " + imageInfos);
 
-
         return "/map/map";
     }
-
-
- 
-
 
 }

a4f683bed18fb6a8a852e810a6534fdd629927f5
hire50 <judahbae0225@gmail.com>
2023-09-06 02:02:13 +0900

    test commit

--- bnb/src/main/java/com/cjcs/bnb/dto/OrderDto.java
+++ bnb/src/main/java/com/cjcs/bnb/dto/OrderDto.java
@@ -1,5 +1,21 @@
 package com.cjcs.bnb.dto;
 
+import java.util.List;
+import com.cjcs.bnb.dao.OrderItemDto;
+import lombok.Data;
+
+@Data
 public class OrderDto {
     
+    // 주문 정보 필드
+    private String orderId;
+    private List<OrderItemDto> orderItems;
+    private double totalAmount;
+
+    // 결제 정보 필드
+    private PaymentDto payment;
+
+    // 배송 정보 필드
+    private ShoppingAddressDto sAddress;
 }
+

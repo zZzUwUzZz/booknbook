c073189a1449de462f315f8d78e3114505cb0369
hire50 <judahbae0225@gmail.com>
2023-09-07 09:06:47 +0900

    브랜치다시만듦

--- bnb/src/main/java/com/cjcs/bnb/controller/OrderController.java
+++ bnb/src/main/java/com/cjcs/bnb/controller/OrderController.java
@@ -33,33 +33,33 @@ public class OrderController {
     private RentalService reSer;
 
 
-    // 주문 페이지로 이동
-    @GetMapping("/checkout")
-    public String checkoutPage(Model model, HttpSession session) {
-        // 주문 페이지 관련 처리
-        // 주문 상품 목록, 배송지 정보 등을 모델에 추가
-        return "checkout"; // checkout.jsp로 이동
-    }
+//     // 주문 페이지로 이동
+//     @GetMapping("/checkout")
+//     public String checkoutPage(Model model, HttpSession session) {
+//         // 주문 페이지 관련 처리
+//         // 주문 상품 목록, 배송지 정보 등을 모델에 추가
+//         return "checkout"; // checkout.jsp로 이동
+//     }
 
-    // 주문 처리
-    @PostMapping("/placeOrder")
-    public String placeOrder(@ModelAttribute("orderDto") OrderDto oDto, Model model, HttpSession session) {
-        // 주문 처리
-        // 주문 정보, 결제 정보, 배송 정보를 받아서 주문을 처리하는 서비스 메서드 호출
-        // 주문 성공 또는 실패에 따라 페이지 이동
-        return "orderSuccess"; // 주문 성공 페이지
-        // 또는
-        // return "orderFailure"; // 주문 실패 페이지
-    }
+//     // 주문 처리
+//     @PostMapping("/placeOrder")
+//     public String placeOrder(@ModelAttribute("orderDto") OrderDto oDto, Model model, HttpSession session) {
+//         // 주문 처리
+//         // 주문 정보, 결제 정보, 배송 정보를 받아서 주문을 처리하는 서비스 메서드 호출
+//         // 주문 성공 또는 실패에 따라 페이지 이동
+//         return "orderSuccess"; // 주문 성공 페이지
+//         // 또는
+//         // return "orderFailure"; // 주문 실패 페이지
+//     }
 
-    // 배송지 정보 변경
-    @PostMapping("/updateShoppingAddress")
-    public String updateShoppingAddress(@ModelAttribute("ShoppingAddressDto") ShoppingAddressDto SADto, Model model, HttpSession session) {
-        // 배송지 정보 변경 처리
-        // 새로운 배송지 정보를 받아서 업데이트
-        return "redirect:/order/checkout"; // 주문 페이지로 리다이렉트
-    }
-}
+//     // 배송지 정보 변경
+//     @PostMapping("/updateShoppingAddress")
+//     public String updateShoppingAddress(@ModelAttribute("ShoppingAddressDto") ShoppingAddressDto SADto, Model model, HttpSession session) {
+//         // 배송지 정보 변경 처리
+//         // 새로운 배송지 정보를 받아서 업데이트
+//         return "redirect:/order/checkout"; // 주문 페이지로 리다이렉트
+//     }
+// }
 //     //의존성주입.. 수정,추가 알아서하셈
 
 //     @Autowired
@@ -71,11 +71,11 @@ public class OrderController {
 
     //여기부터 페이지-메서드 매핑
 
-    // @GetMapping
-    // public String cart() {
+    @GetMapping
+    public String cart() {
 
-    //     return "cart";
-    // }
+        return "cart";
+    }
 
     // @PostMapping  
     // public String cart() {
@@ -94,5 +94,6 @@ public class OrderController {
 
     //     return "order";
     // }
+    }
 
 

dcc8e0a3f7f342af40e9cae2deb735b4eec242ff
Nilla-va <bahnnn@naver.com>
2023-09-09 00:39:14 +0900

    옙

--- bnb/src/main/java/com/cjcs/bnb/dto/SearchDto.java
+++ bnb/src/main/java/com/cjcs/bnb/dto/SearchDto.java
@@ -10,9 +10,9 @@ public class SearchDto {
     
 	private String colname;   // 예: 검색범위가 제목인지 내용인지..
 	private String keyword;   // 검색어
-	private int pageNum;      // 0으로 초기화됨, 보여줄 페이지의 번호
-	private int pageCnt;      // 0으로 초기화됨, 페이지 당 출력할 페이지번호의 개수
-	private int listCnt;      // 0으로 초기화됨, 페이지 당 출력할 글의 개수
+	private int pageNum = 1;      // 보여줄 페이지의 번호
+	private int pageCnt = 5;      // 페이지 당 출력할 페이지번호의 개수
+	private int listCnt = 10;      // 페이지 당 출력할 글의 개수
 	private String listUrl;   // 페이징처리 하려는 뷰페이지의 url공통부분
 
 }

ab6df509d99f3d1505aeab9d2a1d9e6afc8edd94
hire50 <judahbae0225@gmail.com>
2023-09-19 12:24:26 +0900

    병합된거가져옴!

--- bnb/src/main/java/com/cjcs/bnb/service/RentalService.java
+++ bnb/src/main/java/com/cjcs/bnb/service/RentalService.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cjcs.bnb.dao.BookMapper;
 import com.cjcs.bnb.dao.NotificationDao;

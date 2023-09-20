1a644e9f4e749c5cc6ef150dcc352cc47f6fe6d9
zZzUwUzZz <htrue521@gmail.com>
2023-09-20 10:51:23 +0900

    제발..도서목록리스트 (셀러) 수정

--- bnb/src/main/java/com/cjcs/bnb/service/StockService.java
+++ bnb/src/main/java/com/cjcs/bnb/service/StockService.java
@@ -36,12 +36,12 @@ public class StockService {
         return stockMapper.updateStock(stockUpdateDto) > 0;
     }
 
-    public List<BookDto> SellerBookListDT(String s_id, String filter, String keyword) {
-        return stockMapper.SellerBookListDT(s_id, filter, keyword);
+    public List<BookDto> SellerBookListDT(String s_id) {
+        return stockMapper.SellerBookListDT(s_id);
     }
 
-    public BookDto BookInfoDt(String isbn, String sellerId) {
-        return stockMapper.BookInfoDt(isbn, sellerId);
+    public BookDto BookInfoDt(String isbn, String s_id) {
+        return stockMapper.BookInfoDt(isbn, s_id);
     }
 
     public boolean findStockNotif(StockNotifDto stockNotifDto) {
